aa0991778188a6f6e6b49557b6d2288c013cd27a
Michel van Wouw <mvwouw@hotmail.com>
2022-11-23 22:34:30 +0100

    Added commenting

--- fuzzy_matching.py
+++ fuzzy_matching.py
@@ -1,19 +1,21 @@
 """ Fuzzy Matching
-ShortStringToCollection.py: Create collections of short strings and match single strings against them.
+Easy and fast fuzzy (approximate) matching of single strings against large collections of strings.
 
-Create collections of short strings with the SSC class and use the top_col() function to do fast lookups. Give each
-collection its' own string pre-processing settings and a label. Calling top_col() will match a string to multiple
-collections at once and return the top x matching strings from all collections, including extra info and statistics for
-the search.
-Very fast for bulk matching by using numpy's broadcasting feature. Thank you tkarabela (Big Python) for providing the
-code which this is based on.
+Create libraries with the Stringlib class and add collections of strings to them. Some common pre-processing options
+are available and are applied per collection. Also present are some basic library and collection management methods.
 
-Usage with default settings:
-    col1 = SSC(['Netherlands', 'Germany', 'France', 'Spain'], 'world countries')
-    col2 = SSC(['some', 'very', 'long', 'list', '.....'], 'english dictionary')
+Basic usage with default settings:
+    lib1 = StringLib()
+    lib1.add_col(['Netherlands', 'Germany', 'France', 'Spain'], 'world countries')
+    lib1.add_col(['London', 'Edinburgh', 'Manchester', 'Birmingham', 'Glasgow'], 'uk cities')
 
-    results_dict = top_col('testme', [col1, col2])
+    results_dict = lib1.top_from_col('testme')
 
+
+For reference on a 2020 laptop:
+Compares a 28 character string to more than 1.000.000 strings in about 600ms.
+Compares a 10 character string to about 200.000 strings in about 70ms.
+Compares a 6 character string to more than 60.000 strings in about 12ms.
 """
 
 
@@ -23,17 +25,8 @@ from time import perf_counter_ns
 
 
 class StringLib:
-    """ Short String Collection
-    Represents a collection of strings that can be used to match single strings against. Has some options for how
-    strings are pre-processed before matching.
-
-    Arguments:
-        collection: A list of strings to use as this objects' collection
-        label: A string used to identify this collection
-        ignore_case: Specify whether to ignore Case when matching against this collection
-        to_ascii: Specify whether to try and convert any non-ASCII character to ASCII first before matching
-                  (ie: ë to e, etc.)
-        no_strip: Specify whether to strip strings from preceeding and trailing whitespace characters before matching
+    """ String Library
+    Manage collections of strings and run matches against them.
     """
     def __init__(self):
         self.__strlib = {'collections': [], 'total_strings': 0}
@@ -44,7 +37,20 @@ class StringLib:
             total_refs += self.__strlib[collection]['num_ref']
         return f"String Library holding {total_refs} references in {len(self.collections())} collections"
 
-    def add_col(self, collection: list[str], label: str, ignore_case: bool = True, to_ascii: bool = True, no_strip: bool = False):
+    def add_col(self, collection: list[str], label: str,
+                ignore_case: bool = True,
+                to_ascii: bool = True,
+                no_strip: bool = False):
+        """ Add a collection to this library
+
+        :param collection: A list of strings to run matches against.
+        :param label: The name of this collection.
+        :param ignore_case: Specify whether to ignore character cases when matching against this collection.
+        :param to_ascii: Specify whether to try and convert any non-ASCII character to ASCII first before matching
+                         (ie: ë to e, etc.).
+        :param no_strip: Specify whether to strip strings from preceeding and trailing whitespace characters when
+                         matching against this collection.
+        """
         # LBYL checks
         if type(label) is not str:
             raise TypeError(f"'label' argument is not a string: {label}")
@@ -67,14 +73,13 @@ class StringLib:
         self.__strlib[label]['to_ascii'] = to_ascii
         self.__strlib[label]['no_strip'] = no_strip
 
-        # Collection pre-processing
+        # Pre-processing
         if not no_strip:
             collection = [item.strip() for item in collection]
         while "" in collection:
             collection.remove("")
         while "\n" in collection:
             collection.remove("\n")
-
         if to_ascii:
             if ignore_case:
                 reference = [normalize("NFKD", item).encode("ascii", "ignore").decode().lower() for item in collection]
@@ -96,12 +101,19 @@ class StringLib:
             self.__strlib[label]['col_by_len'].setdefault(len(tup[2]), []).append(tup)
 
     def del_col(self, label: str):
+        """ Delete a collection from this library
+        :param label: Name of the collection to be deleted.
+        """
         if type(label) is not str:
             raise TypeError(f"'label' argument is not a string: {label}")
         self.__strlib.pop(label, None)
         self.__strlib['collections'].remove(label)
 
     def ren_col(self, old_label: str, new_label: str):
+        """ Rename a collection
+        :param old_label: Current name of the collection.
+        :param new_label: New name of the collection.
+        """
         if type(old_label) is not str:
             raise TypeError(f"'old_label' argument is not a string: {old_label}")
         if type(new_label) is not str:
@@ -114,6 +126,11 @@ class StringLib:
             print(f"\nError: Collection not found: {old_label}")
 
     def col_info(self, label: str, full: bool = False) -> dict | None:
+        """ Get information about a collection
+        :param label: Name of the collection.
+        :param full: When True returns the string list aswell.
+        :return: A dict containing information about the collection. Returns None if the library is empty.
+        """
         if type(label) is not str:
             raise TypeError(f"'label' argument is not a string: {label}")
         cols = self.collections()
@@ -136,13 +153,21 @@ class StringLib:
         except KeyError:
             print(f"\nError: Collection not found: {label}")
 
-    def collections(self):
+    def collections(self) -> list[str]:
+        """ Get a list of collection names in this library
+        :return: A list of collection names in this library.
+        """
         return self.__strlib['collections']
 
     def clr_lib(self):
+        """ Clear this library
+        """
         self.__strlib = {'collections': [], 'total_strings': 0}
 
     def lib_info(self) -> dict:
+        """ Get information about this library
+        :return: A dict with information about this library.
+        """
         collections = self.collections()
         info = {'collections': collections}
         tot_refs = 0
@@ -156,14 +181,18 @@ class StringLib:
         info['total_strings'] = tot_refs
         return info
 
-    def top_from_col(self, sample: str, collections: list[str] = None, top: int = 1, look_around: int = -1) -> dict | None:
-        """
-        Takes a sample string and one or more collections and returns the top x best matches.
-        :param sample: string to match against collection(s)
-        :param collections: a list of SSC objects to match against
-        :param top: the amount of best matches to return
-        :param look_around: set the maximum amount of charaters
-        :return: a dictionary with results and some stats
+    def top_from_col(self, sample: str,
+                     collections: list[str] = None,
+                     top: int = 1,
+                     look_around: int = -1
+                     ) -> dict | None:
+        """ Get the top x best matches
+        :param sample: A string to match against one or more collections from this library.
+        :param collections: A list of collection names to match against. If None uses all collections.
+        :param top: The amount of best matches to return
+        :param look_around: Set a limit to how many characters longer or shorter a (collection-) string may be before
+               skipping it. Set to -1 for no limit.
+        :return: A dictionary with results and some stats about the search.
         """
         # LBYL checks
         if sample.strip() == "" or sample is None:
@@ -187,7 +216,6 @@ class StringLib:
         if look_around < -1:
             raise ValueError(f"'look_around' argument may not be smaller than -1: ({look_around} < -1)")
 
-        # initialize results dictionary
         results = {'sample': sample, 'skipped': 0, 'total': 0}
         st_t0 = perf_counter_ns()
 
@@ -207,12 +235,13 @@ class StringLib:
             # do matching per word length
             len_sample = len(sample)
             for length, ref_list in self.__strlib[collection]['col_by_len'].items():
-                # skip set if character length is out of look_around range
+                # skip string set if character length is out of look_around range
                 if look_around > -1:
                     if abs(length - len_sample) > look_around:
                         results['skipped'] += len(self.__strlib[collection]['col_by_len'][length])
                         continue
 
+                # get a list ratios for this string set
                 ratios = [ratio(sample, ref[2]) for ref in ref_list]
 
                 # skim intermediate results to save time sorting
